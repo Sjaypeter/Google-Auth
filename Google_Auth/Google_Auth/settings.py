3c25dcfeca8a14516a25d082e697bf94ee0fe281
Sjayp <Sjaypeter.sjp@gmail.com>
2026-01-16 11:29:44 +0100

    jwt urls and config

--- Google_Auth/Google_Auth/settings.py
+++ Google_Auth/Google_Auth/settings.py
@@ -118,3 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'core.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        )
+}
